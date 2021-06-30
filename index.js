f28c67d660ae686e2872526b196b34f70669ccfb
bailey watkins <bailey.watkins@rocketmail.com>
2021-06-30 15:15:00 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31
+const num2 =  2
+const multiply = num1*num2
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+const max = Math.max(1, 2, 3, 4, 5, 10, 20);
